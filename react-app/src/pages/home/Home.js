cdaf67cd7a6bd0a832c4f255ccc451236aa35311
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-02-21 19:55:57 +0530

    Modification
    -> Remove unwanted comments
    -> Change Design of Home Page

--- react-app/src/pages/home/Home.js
+++ react-app/src/pages/home/Home.js
@@ -3,15 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center bg-gray-100 min-h-screen">
-      <div className="bg-white py-20">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold text-gray-900">Welcome to Our Website</h1>
-            <p className="mt-4 text-lg text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <div className="mt-6">
-              <Link to="/login" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-3 rounded-md">Get Started</Link>
-            </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white-100">
+      <div className="bg-white py-20 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-gray-900">Welcome to Our Website</h1>
+          <p className="mt-4 text-lg text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <div className="mt-6">
+            <Link to="/login" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-3 rounded-md">Get Started</Link>
           </div>
         </div>
       </div>
