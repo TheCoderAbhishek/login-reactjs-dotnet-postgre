c27f9d9cedc474c98d09197127525373b91dd832
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-02-23 08:12:39 +0530

    Made changes in design

--- react-app/src/pages/home/Home.js
+++ react-app/src/pages/home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
           <h1 className="text-4xl font-bold text-gray-900">Welcome to Our Website</h1>
           <p className="mt-4 text-lg text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           <div className="mt-6">
-            <Link to="/login" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-3 rounded-md">Get Started</Link>
+            <Link to="/login" className="inline-block bg-indigo-500 hover:bg-blue-600 text-white font-semibold px-6 py-3 rounded-md">Get Started</Link>
           </div>
         </div>
       </div>
