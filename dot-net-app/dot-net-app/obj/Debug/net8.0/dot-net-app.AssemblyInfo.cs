f1c6af5fe6682849fd35cd867f4580fe30f83119
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-02-28 20:27:09 +0530

    system files update

--- dot-net-app/dot-net-app/obj/Debug/net8.0/dot-net-app.AssemblyInfo.cs
+++ dot-net-app/dot-net-app/obj/Debug/net8.0/dot-net-app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dot-net-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc40bfb893f2f143c381ecc6926990afdb03b311")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b0a31190bf56d7c8ed2c4f351dfffa75972e9e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("dot-net-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dot-net-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
